4a63aa67ca651711d0efa743433e7dfaa5e4b2f0
Dominik Matysek <63716538+dominik-matysek@users.noreply.github.com>
2023-03-15 14:46:03 +0100

    Delete Matysek.Dominik.cw1.final/Matysek.Dominik.cs1.final/target directory

--- Matysek.Dominik.cw1.final/Matysek.Dominik.cs1.final/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main.project"},{"l":"polsl.controller.eng"},{"l":"polsl.model.eng"},{"l":"polsl.view.eng"}];updateSearchResults();
